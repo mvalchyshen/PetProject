5da3fc72630381f611572c852a940ea81991b1ab
Yevhenii Kuzmenko <zheniakuzma@gmail.com>
2021-10-12 19:33:31 +0300

    Added Logging

--- src/main/java/ua/petproject/repositories/CrudInterface.java
+++ src/main/java/ua/petproject/repositories/CrudInterface.java
@@ -1,5 +1,7 @@
 package ua.petproject.repositories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.petproject.annotations.Column;
 import ua.petproject.annotations.Table;
 import ua.petproject.util.DataBaseConnection;
@@ -14,53 +16,84 @@ import java.util.List;
 
 public interface CrudInterface<T, ID> {
 
+    String CHARACTER = "CHARACTER";
+    String BYTE = "BYTE";
+    String STRING = "STRING";
+    String BIG_DECIMAL = "BIG DECIMAL";
+    String SHORT = "SHORT";
+    String INTEGER = "INTEGER";
+    String LONG = "LONG";
+    String FLOAT = "FLOAT";
+    String DOUBLE = "DOUBLE";
+    String BYTE_ARRAY = "BYTE[]";
+    String SQL_DATE = "SQL.DATE";
+    String TIME = "TIME";
+    String TIMESTAMP = "TIMESTAMP";
+    String BOOLEAN = "BOOLEAN";
+    String INPUT_STREAM = "INPUT_STREAM";
+    String SQL_BLOB = "SQL_BLOB";
+    String SQL_CLOB = "SQL_CLOB";
+    Logger logger = LoggerFactory.getLogger(CrudInterface.class);
+
     DataBaseConnection dbConnection = new DataBaseConnection();
     Connection connection = dbConnection.getConnection();
 
     static void setObject(PreparedStatement st, int parameterIndex, Object parameterObj) throws SQLException {
 
         if (parameterObj == null) {
-            st.setNull(parameterIndex, Types.OTHER);
+            st.setNull(parameterIndex, java.sql.Types.OTHER);
         } else {
             if (parameterObj instanceof Character) {
-                System.out.println("CHARACTER");
+                logger.info(CHARACTER);
                 st.setString(parameterIndex, String.valueOf(parameterObj));
             } else if (parameterObj instanceof Byte) {
-                System.out.println("BYTE");
+                logger.info(BYTE);
                 st.setInt(parameterIndex, ((Byte) parameterObj).intValue());
             } else if (parameterObj instanceof String) {
-                System.out.println("STRING");
+                logger.info(STRING);
                 st.setString(parameterIndex, (String) parameterObj);
             } else if (parameterObj instanceof BigDecimal) {
-                System.out.println("BYTE");
+                logger.info(BIG_DECIMAL);
                 st.setBigDecimal(parameterIndex, (BigDecimal) parameterObj);
             } else if (parameterObj instanceof Short) {
+                logger.info(SHORT);
                 st.setShort(parameterIndex, ((Short) parameterObj).shortValue());
             } else if (parameterObj instanceof Integer) {
-                System.out.println("INTEGER");
+                logger.info(INTEGER);
                 st.setInt(parameterIndex, ((Integer) parameterObj).intValue());
             } else if (parameterObj instanceof Long) {
+                logger.info(LONG);
                 st.setLong(parameterIndex, ((Long) parameterObj).longValue());
             } else if (parameterObj instanceof Float) {
+                logger.info(FLOAT);
                 st.setFloat(parameterIndex, ((Float) parameterObj).floatValue());
             } else if (parameterObj instanceof Double) {
+                logger.info(DOUBLE);
                 st.setDouble(parameterIndex, ((Double) parameterObj).doubleValue());
             } else if (parameterObj instanceof byte[]) {
+                logger.info(BYTE_ARRAY);
                 st.setBytes(parameterIndex, (byte[]) parameterObj);
-            } else if (parameterObj instanceof Date) {
-                st.setDate(parameterIndex, (Date) parameterObj);
+            } else if (parameterObj instanceof java.sql.Date) {
+                logger.info(SQL_DATE);
+                st.setDate(parameterIndex, (java.sql.Date) parameterObj);
             } else if (parameterObj instanceof Time) {
+                logger.info(TIME);
                 st.setTime(parameterIndex, (Time) parameterObj);
             } else if (parameterObj instanceof Timestamp) {
+                logger.info(TIMESTAMP);
                 st.setTimestamp(parameterIndex, (Timestamp) parameterObj);
             } else if (parameterObj instanceof Boolean) {
+                logger.info(BOOLEAN);
                 st.setBoolean(parameterIndex, ((Boolean) parameterObj).booleanValue());
             } else if (parameterObj instanceof InputStream) {
+                logger.info(INPUT_STREAM);
                 st.setBinaryStream(parameterIndex, (InputStream) parameterObj, -1);
-            } else if (parameterObj instanceof Blob) {
-                st.setBlob(parameterIndex, (Blob) parameterObj);
-            } else if (parameterObj instanceof Clob) {
-                st.setClob(parameterIndex, (Clob) parameterObj);
+            } else if (parameterObj instanceof java.sql.Blob) {
+                logger.info(SQL_BLOB);
+                st.setBlob(parameterIndex, (java.sql.Blob) parameterObj);
+            } else if (parameterObj instanceof java.sql.Clob) {
+                logger.info(SQL_CLOB);
+                st.setClob(parameterIndex, (java.sql.Clob) parameterObj);
             }
         }
     }
